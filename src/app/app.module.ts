3ccb508ad3b0e9ed94cc1cb2d0ae49380371fa12
Alextony-cloud <alextonyrodrigues@gmail.com>
2024-03-07 12:31:34 -0300

    metodo create implementado para categoria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,17 @@ import { MatIconModule } from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
 import {MatTableModule} from '@angular/material/table';
 import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
 import { HomeComponent } from './components/views/home/home.component';
 import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
+import { CategoriaCreateComponent } from './components/views/categoria/categoria-create/categoria-create.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -25,7 +30,8 @@ import { CategoriaReadComponent } from './components/views/categoria/categoria-r
     FooterComponent,
     NavComponent,
     HomeComponent,
-    CategoriaReadComponent
+    CategoriaReadComponent,
+    CategoriaCreateComponent
   ],
   imports: [
     BrowserModule,
@@ -37,8 +43,12 @@ import { CategoriaReadComponent } from './components/views/categoria/categoria-r
     MatIconModule,
     MatCardModule,
     MatTableModule,
+    MatSnackBarModule,
+    MatFormFieldModule,
     HttpClientModule,
-    MatButtonModule
+    MatButtonModule,
+    FormsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/views/categoria/categoria-create/categoria-create.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { CategoriaService } from '../categoria.service';
+import { Categoria } from '../categoria.model';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-categoria-create',
+  templateUrl: './categoria-create.component.html',
+  styleUrls: ['./categoria-create.component.css']
+})
+export class CategoriaCreateComponent implements OnInit{
+ 
+  categoria: Categoria ={
+    nome: '',
+    descricao: ''
+  }
+
+  constructor(private service: CategoriaService, private router: Router){
+
+  }
+ 
+  ngOnInit(): void {
+   
+  }
+
+  create(): void {
+    this.service.create(this.categoria).subscribe((r) => {
+      this.router.navigate(['categorias']);
+      this.service.mensagem('Categoria criada com sucesso!');
+    }, e => {
+      for(let i = 0; i < e.error.errors.length; i++){
+        this.service.mensagem(e.error.errors[i].message)
+      }
+    })
+  }
+
+}
